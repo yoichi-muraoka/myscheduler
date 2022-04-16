b567c3e44239deb463a0eaed669e2dad9b46a3e4
Muraoka <ym.animator@gmail.com>
2022-04-16 12:07:39 +0900

    init

---
+++ src/main/java/com/example/myscheduler/MyschedulerApplication.java
@@ -0,0 +1,13 @@
+package com.example.myscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyschedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyschedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/myscheduler/MyschedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyschedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
