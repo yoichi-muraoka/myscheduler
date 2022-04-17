5f910234afcaea9c0d05a834cd55ab0b2ed86f57
Muraoka <ym.animator@gmail.com>
2022-04-17 17:16:53 +0900

    前月と翌月に表示日数を算出

--- src/main/java/com/example/myscheduler/controller/ScheduleController.java
+++ src/main/java/com/example/myscheduler/controller/ScheduleController.java
@@ -1,6 +1,7 @@
 package com.example.myscheduler.controller;
 
 import com.example.myscheduler.service.ScheduleService;
+import com.example.myscheduler.util.YearMonthUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,7 +16,12 @@ public class ScheduleController {
     @GetMapping
     public String index(Model model) {
         String yearMonth = "2022-04";
+        // 当月のスケジュール
         model.addAttribute("schedule", scheduleService.getMonthly(yearMonth));
+        // 月初と月末の曜日情報(整数)
+        model.addAttribute("daysOfWeeks", YearMonthUtil.getFirstAndLastDayOfWeek(yearMonth));
+        // 前月の末日
+        model.addAttribute("lastDateOfPrevMonth", YearMonthUtil.getLastDateOfPrevMonth(yearMonth));
         return "index";
     }
 
--- src/main/java/com/example/myscheduler/util/YearMonthUtil.java
+++ src/main/java/com/example/myscheduler/util/YearMonthUtil.java
@@ -27,6 +27,28 @@ public class YearMonthUtil {
     }
 
     /**
+     * 月初と月末の曜日を整数で返す (月=1, 火=2, ..., 日=7)
+     * @param yearMonth 年月。「2022-04」のような文字列形式
+     * @return 曜日番号の配列 [0]月初, [1]月末
+     */
+    public static int[] getFirstAndLastDayOfWeek(String yearMonth) {
+        YearMonth ym = YearMonth.parse(yearMonth);
+        // 月初と月末の曜日を数値で取得 (月=1, 火=2, ..., 日=7)
+        int first = ym.atDay(1).getDayOfWeek().getValue();
+        int last = ym.atEndOfMonth().getDayOfWeek().getValue();
+        return new int[] {first, last};
+    }
+
+    /**
+     * 前月の末日を返す
+     * @param yearMonth 年月。「2022-04」のような文字列形式
+     * @return 末日
+     */
+    public static int getLastDateOfPrevMonth(String yearMonth) {
+        return YearMonth.parse(yearMonth).minusMonths(1).lengthOfMonth();
+    }
+
+    /**
      * 年月を 年と月 に分割
      * @param yearMonth 年月。「2022-04」のような文字列形式
      * @return [0]年, [1]月
