253129557f717ceea477d2e0f0070ed4cb891cab
Muraoka <ym.animator@gmail.com>
2022-04-17 11:54:58 +0900

    予定をカレンダーに表示

--- src/main/java/com/example/myscheduler/controller/ScheduleController.java
+++ src/main/java/com/example/myscheduler/controller/ScheduleController.java
@@ -1,13 +1,21 @@
 package com.example.myscheduler.controller;
 
+import com.example.myscheduler.service.ScheduleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ScheduleController {
 
+    @Autowired
+    private ScheduleService scheduleService;
+
     @GetMapping
-    public String index() {
+    public String index(Model model) {
+        String yearMonth = "2022-04";
+        model.addAttribute("schedule", scheduleService.getMonthly(yearMonth));
         return "index";
     }
 
