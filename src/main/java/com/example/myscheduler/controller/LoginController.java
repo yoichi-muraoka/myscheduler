ab4a4286de5bec179e722e550009b31257deeb3d
Muraoka <muraoka@villhill.net>
2022-04-18 12:48:02 +0900

    ログインフォームを通じたログインの実装

--- src/main/java/com/example/myscheduler/controller/LoginController.java
+++ src/main/java/com/example/myscheduler/controller/LoginController.java
@@ -1,14 +1,52 @@
 package com.example.myscheduler.controller;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.myscheduler.domain.User;
+import com.example.myscheduler.service.LoginService;
 
 @Controller
 public class LoginController {
 
+	@Autowired
+	private LoginService loginService;
+	
     @GetMapping("/login")
-    public String login() {
+    public String login(Model model) {
+    	model.addAttribute("user", new User());
         return "login";
+    }
+    
+    @PostMapping("/login")
+    public String login(
+    		@Valid User user,
+    		Errors errors) {
+    	
+    	// 未入力
+    	if(errors.hasErrors()) {
+    		return "login";
+    	}
+    	
+    	// 不正なIDまたはパスワード
+    	if(!loginService.isCorrectIdPass(user.getLoginId(), user.getLoginPass())) {
+    		errors.rejectValue("loginId", "incorrect.login.info");
+    		return "login";
+    	}
+    	
+    	return "redirect:/";
+    }
+    
+    @GetMapping("/logout")
+    public String logout() {
+    	loginService.logout();
+    	return "redirect:/login";
     }
 
 }
